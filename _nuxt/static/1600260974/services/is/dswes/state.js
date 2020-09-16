f9bc38dabf1d822317b71c703e82a97684d133d6
dragoonzx <dragoonz@yandex.ru>
2020-09-16 15:56:16 +0300

    1ec0baa

---
+++ _nuxt/static/1600260974/services/is/dswes/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1600260974",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{content:{content:{}}},serverRendered:false,routePath:a,config:{axios:{browserBaseURL:void 0}}}}("\u002Fservices\u002Fis\u002Fdswes"));
